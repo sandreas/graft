b4f842f4bf9dc993dfac3a6dcaf6a34491226485
Andreas <andreas.schroden@gmail.com>
2017-08-01 08:29:48 +0200

    Added DestinationPattern for Transfer

--- newfile/transfer.go
+++ newfile/transfer.go
@@ -63,13 +63,121 @@ func (t *Transfer) Find(matcher *newmatcher.CompositeMatcher) {
 }
 
 
-func (t *Transfer) CopyTo(dst *newpattern.BasePattern) {
+func (t *Transfer) CopyTo(dst *newpattern.DestinationPattern) {
 
 	if dst.IsFile() {
 
 	}
 
 }
+
+
+//func (p *DestinationPattern) BuildDestination(element, patternPath string ) (string) {
+//	var dst string
+//	if p.Pattern == "" {
+//		dst = filepath.ToSlash(p.Path + element[len(patternPath) + 1:])
+//	} else {
+//		dst = compiledPattern.ReplaceAllString(pattern.NormalizeDirSep(element), pattern.NormalizeDirSep(destinationPattern))
+//	}
+//
+//	return dst
+//}
+
+/*
+dstPath, dstPatt := pattern.ParsePathPattern(destinationPattern)
+
+// replace $1_ with ${1}_ to prevent problems
+dollarUnderscore, _ := regexp.Compile("\\$([1-9][0-9]*)_")
+destinationPattern = dollarUnderscore.ReplaceAllString(destinationPattern, "${$1}_")
+
+var dst string
+for _, element := range matchingPaths {
+	if dstPatt == "" {
+		dst = pattern.NormalizeDirSep(dstPath + element[len(patternPath) + 1:])
+	} else {
+		dst = compiledPattern.ReplaceAllString(pattern.NormalizeDirSep(element), pattern.NormalizeDirSep(destinationPattern))
+	}
+	transferElementHandler(element, dst)
+}
+
+func transferElementHandler() {
+
+	prntln(src + " => " + dst)
+
+	if *dryRun {
+		return
+	}
+
+	srcStat, srcErr := os.Stat(src)
+
+	if srcErr != nil {
+		prntln("could not read source: ", srcErr)
+		return
+	}
+
+	dstStat, _ := os.Stat(dst)
+	dstExists := file.Exists(dst)
+	if srcStat.IsDir() {
+		if ! dstExists {
+			if os.MkdirAll(dst, srcStat.Mode()) != nil {
+				prntln("Could not create destination directory")
+			}
+			appendRemoveDir(dst)
+			fixTimes(dst, srcStat)
+			return
+		}
+
+		if dstStat.IsDir() {
+			appendRemoveDir(dst)
+			fixTimes(dst, srcStat)
+			return
+		}
+
+		prntln("destination already exists as file, source is a directory")
+		return
+	}
+
+	if dstExists && dstStat.IsDir() {
+		prntln("destination already exists as directory, source is a file")
+		return
+	}
+
+	srcDir := path.Dir(src)
+	srcDirStat, _ := os.Stat(srcDir)
+
+	dstDir := path.Dir(dst)
+	if ! file.Exists(dstDir) {
+		os.MkdirAll(dstDir, srcDirStat.Mode())
+	}
+
+	if *move {
+		renameErr := os.Rename(src, dst)
+		if renameErr == nil {
+			appendRemoveDir(srcDir)
+			fixTimes(dst, srcStat)
+			return
+		}
+		prntln("Could not rename source")
+		return
+	}
+
+	srcPointer, srcPointerErr := os.Open(src)
+	if srcPointerErr != nil {
+		prntln("Could not open source file")
+		return
+	}
+	dstPointer, dstPointerErr := os.OpenFile(dst, os.O_WRONLY | os.O_CREATE, srcStat.Mode())
+
+	if dstPointerErr != nil {
+		prntln("Could not create destination file", dstPointerErr.Error())
+		return
+	}
+
+	file.CopyResumed(srcPointer, dstPointer, handleProgress)
+	fixTimes(dst, srcStat)
+}
+ */
+
 
 //func (t *Transfer) moveTo(dst string) {
 //
--- newpattern/destination.go
+++ newpattern/destination.go
@@ -1,96 +1,22 @@
 package newpattern
 
+import (
+	"regexp"
+)
 
-/*
-dstPath, dstPatt := pattern.ParsePathPattern(destinationPattern)
-
-// replace $1_ with ${1}_ to prevent problems
-dollarUnderscore, _ := regexp.Compile("\\$([1-9][0-9]*)_")
-destinationPattern = dollarUnderscore.ReplaceAllString(destinationPattern, "${$1}_")
-
-var dst string
-for _, element := range matchingPaths {
-	if dstPatt == "" {
-		dst = pattern.NormalizeDirSep(dstPath + element[len(patternPath) + 1:])
-	} else {
-		dst = compiledPattern.ReplaceAllString(pattern.NormalizeDirSep(element), pattern.NormalizeDirSep(destinationPattern))
-	}
-	transferElementHandler(element, dst)
+type DestinationPattern struct {
+	BasePattern
 }
 
+func NewDestinationPattern(patternString string) *DestinationPattern {
+	destinationPattern := &DestinationPattern{}
+	destinationPattern.parse(patternString)
+	destinationPattern.fixRegex()
+	return destinationPattern
+}
 
-
-	prntln(src + " => " + dst)
-
-	if *dryRun {
-		return
-	}
-
-	srcStat, srcErr := os.Stat(src)
-
-	if srcErr != nil {
-		prntln("could not read source: ", srcErr)
-		return
-	}
-
-	dstStat, _ := os.Stat(dst)
-	dstExists := file.Exists(dst)
-	if srcStat.IsDir() {
-		if ! dstExists {
-			if os.MkdirAll(dst, srcStat.Mode()) != nil {
-				prntln("Could not create destination directory")
-			}
-			appendRemoveDir(dst)
-			fixTimes(dst, srcStat)
-			return
-		}
-
-		if dstStat.IsDir() {
-			appendRemoveDir(dst)
-			fixTimes(dst, srcStat)
-			return
-		}
-
-		prntln("destination already exists as file, source is a directory")
-		return
-	}
-
-	if dstExists && dstStat.IsDir() {
-		prntln("destination already exists as directory, source is a file")
-		return
-	}
-
-	srcDir := path.Dir(src)
-	srcDirStat, _ := os.Stat(srcDir)
-
-	dstDir := path.Dir(dst)
-	if ! file.Exists(dstDir) {
-		os.MkdirAll(dstDir, srcDirStat.Mode())
-	}
-
-	if *move {
-		renameErr := os.Rename(src, dst)
-		if renameErr == nil {
-			appendRemoveDir(srcDir)
-			fixTimes(dst, srcStat)
-			return
-		}
-		prntln("Could not rename source")
-		return
-	}
-
-	srcPointer, srcPointerErr := os.Open(src)
-	if srcPointerErr != nil {
-		prntln("Could not open source file")
-		return
-	}
-	dstPointer, dstPointerErr := os.OpenFile(dst, os.O_WRONLY | os.O_CREATE, srcStat.Mode())
-
-	if dstPointerErr != nil {
-		prntln("Could not create destination file", dstPointerErr.Error())
-		return
-	}
-
-	file.CopyResumed(srcPointer, dstPointer, handleProgress)
-	fixTimes(dst, srcStat)
- */
+// 	replace $1_ with ${1}_ to prevent problems during rename
+func (p *DestinationPattern) fixRegex() {
+	dollarUnderscore, _ := regexp.Compile("\\$([1-9][0-9]*)_")
+	p.Pattern = dollarUnderscore.ReplaceAllString(p.Pattern, "${$1}_")
+}
