36b1fa783d34d23803fa6bcf9a49fe1459ac3e7d
Andreas <andreas.schroden@gmail.com>
2017-08-02 17:14:23 +0200

    Added observer for file_copier test

--- newtransfer/file_copier_test.go
+++ newtransfer/file_copier_test.go
@@ -4,7 +4,19 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/spf13/afero"
+	"github.com/sandreas/graft/newdesignpattern/observer"
+	"time"
 )
+
+type FakeObserver struct {
+	newdesignpattern.ObserverInterface
+	messages []string
+}
+
+func(ph *FakeObserver) Notify(a...interface{}){
+	ph.messages = append(ph.messages, a[0].(string))
+}
+
 
 func prepareFilesystemTest(src, srcContent, dst, dstContent string) afero.Fs {
 	appFS := afero.NewMemMapFs()
@@ -20,6 +32,9 @@ func TestCopyNewFile(t *testing.T) {
 	expect := assert.New(t)
 
 	subject := NewFileCopier()
+	subject.ProgressHandler = NewCopyProgressHandler(2, 1 * time.Nanosecond)
+	observer := &FakeObserver{}
+	subject.RegisterObserver(observer)
 
 	srcFile := "test1-src.txt"
 	srcContents := "this is a file without existing destination"
@@ -28,9 +43,12 @@ func TestCopyNewFile(t *testing.T) {
 	subject.Fs = prepareFilesystemTest(srcFile, srcContents, destinationFile, "")
 	err := subject.Copy(srcFile, destinationFile)
 	expect.Equal(nil, err)
-	dstContents, _ := afero.ReadFile(subject.Fs, destinationFile)
 
+	dstContents, _ := afero.ReadFile(subject.Fs, destinationFile)
 	expect.Equal(srcContents, string(dstContents))
+
+	expect.Len(observer.messages, 1)
+	expect.Equal("\r[====================>] 100.00%   0.00/s\n", observer.messages[0])
 }
 
 func TestCopyLargerSourceError(t *testing.T) {
