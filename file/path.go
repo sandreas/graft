14d2be94516937daa36625357a03d558e6e0b162
Andreas <andreas.schroden@gmail.com>
2017-08-15 20:21:20 +0200

    Added path struct

--- file/path.go
+++ file/path.go
@@ -15,58 +15,85 @@ import (
 //:type
 
 const (
-	typeNotExisting = 0
-	//typeDir         = 1
-	//typeFile        = 2
+	typeDir  = 1
+	typeFile = 2
+	separator = string(os.PathSeparator)
 )
 
 type Path struct {
 	fmt.Stringer
 	volume   string
 	path     string
+	name     string
 	kind     int
 	absolute bool
-	stat     os.FileInfo
 }
 
 func NewPath(path string) *Path {
+
+
+	vol := filepath.VolumeName(path)
+	path = strings.TrimPrefix(path, vol)
+	kind := getPathType(path)
+	path = filepath.Clean(path)
+	name := ""
+
+	if kind == typeFile {
+		dir := filepath.Dir(path) + separator
+		name = strings.TrimPrefix(path, dir)
+		path = strings.TrimSuffix(dir, separator)
+	}
+	if path == "." {
+		path = ""
+	}
+
 	p := &Path{
-		volume:   filepath.VolumeName(path),
-		path:     filepath.Clean(path),
-		kind:     typeNotExisting,
+		volume:   filepath.FromSlash(vol),
+		path:     path,
+		name:	name,
+		kind:     kind,
 		absolute: false,
 	}
+
 	p.normalize()
 	return p
 }
 
+func getPathType(path string) int {
+	if strings.HasSuffix(path, "/") || strings.HasSuffix(path, "\\") || path == "" {
+		return typeDir
+	}
+	return typeFile
+}
+
 func (path *Path) normalize() {
-	dirsep := string(os.PathSeparator)
-	if path.volume != "" || strings.HasPrefix(path.path, dirsep) {
+	if path.volume != "" || strings.HasPrefix(path.path, separator) {
 		path.absolute = true
 		path.volume = strings.TrimRight(path.volume, "\\/")
 	} else {
-		path.path = strings.TrimPrefix(path.path, "."+dirsep)
+		path.path = strings.TrimPrefix(path.path, "."+separator)
 	}
 }
 
 func (path *Path) build() string {
 	if path.absolute {
-		return path.volume + path.path
+		return path.volume + path.path + separator + path.name
 	}
-	return path.path
-}
-
-func (path *Path) Stat() (os.FileInfo, error) {
-	var err error
-	path.stat, err = os.Stat(path.build())
-	if os.IsNotExist(err) {
-		path.kind = typeNotExisting
-		path.stat = nil
-	}
-	return path.stat, err
+	return path.path + separator + path.name
 }
 
 func (path *Path) String() string {
 	return path.build()
+}
+
+func (path *Path) IsDir() bool {
+	return path.kind == typeDir
+}
+
+func (path *Path) IsFile() bool {
+	return path.kind == typeFile
+}
+
+func (path *Path) IsAbs() bool {
+	return path.absolute
 }
--- file/path_test.go
+++ file/path_test.go
@@ -8,15 +8,41 @@ import (
 func TestNewPath(t *testing.T) {
 	expect := assert.New(t)
 
-	p := NewPath("../data/fixtures")
-	expect.Equal("../data/fixtures", p.String())
+	p := NewPath("../data/test.txt")
+	expect.Equal(".."+separator+"data"+separator+"test.txt", p.String())
+	expect.True(p.IsFile())
+	expect.False(p.IsDir())
+	expect.False(p.IsAbs())
+
 
 	p = NewPath("/tmp/test")
-	expect.Equal("/tmp/test", p.String())
+	expect.Equal(""+separator+"tmp"+separator+"test", p.String())
+	expect.True(p.IsFile())
+	expect.False(p.IsDir())
+	expect.True(p.IsAbs())
 
-	p = NewPath("./data/fixtures")
-	expect.Equal("data/fixtures", p.String())
+
+	p = NewPath("./data/test.txt")
+	expect.Equal("data"+separator+"test.txt", p.String())
+	expect.True(p.IsFile())
+	expect.False(p.IsDir())
+	expect.False(p.IsAbs())
 
 	p = NewPath("./data/fixtures/")
-	expect.Equal("data/fixtures", p.String())
+	expect.Equal("data"+separator+"fixtures"+separator, p.String())
+	expect.False(p.IsFile())
+	expect.True(p.IsDir())
+	expect.False(p.IsAbs())
+
+	p = NewPath(".\\data//fixtures\\")
+	expect.Equal("data"+separator+"fixtures"+separator+"", p.String())
+	expect.False(p.IsFile())
+	expect.True(p.IsDir())
+	expect.False(p.IsAbs())
+
+	//p := NewPath("\\\\tmp\\test\\uncpath.txt")
+	//expect.Equal(separator+separator+"tmp"+separator+"test"+separator+"uncpath.txt", p.String())
+	//expect.False(p.IsFile())
+	//expect.True(p.IsDir())
+	//expect.True(p.IsAbs())
 }
