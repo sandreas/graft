edea0105430fca3e13f31e7fe1c7b9c934d9de73
andreas <andreas.schroden@gmail.com>
2017-09-02 17:36:58 +0200

    Fixed unused imports in unfinished receive command

--- action/receive.go
+++ action/receive.go
@@ -2,60 +2,56 @@ package action
 
 import (
 	"github.com/urfave/cli"
-	"fmt"
-	"github.com/oleksandr/bonjour"
-	"log"
-	"os"
-	"time"
 )
 
 type ReceiveAction struct {
-	AbstractAction
+	*AbstractAction
 }
 
 func (action *ReceiveAction) Execute(c *cli.Context) error {
 
 	// action.PrepareExecution(c, 1, defaultsForPositionalArguments)
 
-	action.PrepareExecution(c, 1, "*")
-
-	if action.CliContext.String("host") == "" {
-		action.lookupServiceAndReceive()
-	} else {
-		action.receive()
-	}
+	// TODO
+	//action.PrepareExecution(c, 1, "*")
+	//
+	//if action.CliContext.String("host") == "" {
+	//	action.lookupServiceAndReceive()
+	//} else {
+	//	action.receive()
+	//}
 	return nil
 }
-func (action *ReceiveAction) receive() error {
-	if err := action.locateSourceFiles(); err != nil {
-		return cli.NewExitError(err.Error(), ErrorLocateSourceFiles)
-	}
-	return nil
-}
-func (action *ReceiveAction) lookupServiceAndReceive() {
-	resolver, err := bonjour.NewResolver(nil)
-	if err != nil {
-		log.Println("Failed to initialize resolver:", err.Error())
-		os.Exit(1)
-	}
-
-	results := make(chan *bonjour.ServiceEntry)
-
-	go func(results chan *bonjour.ServiceEntry, exitCh chan<- bool) {
-		for e := range results {
-			fmt.Printf("%v", e)
-			action.CliContext.Set("host", e.HostName)
-			action.CliContext.Set("port", string(e.Port))
-			action.receive()
-			exitCh <- true
-			time.Sleep(1e9)
-			os.Exit(0)
-		}
-	}(results, resolver.Exit)
-
-	err = resolver.Lookup("graft", "_graft._tcp", "", results)
-	if err != nil {
-		log.Println("Failed to browse:", err.Error())
-	}
-	select {}
-}
+//func (action *ReceiveAction) receive() error {
+//	if err := action.locateSourceFiles(); err != nil {
+//		return cli.NewExitError(err.Error(), ErrorLocateSourceFiles)
+//	}
+//	return nil
+//}
+//func (action *ReceiveAction) lookupServiceAndReceive() {
+//	resolver, err := bonjour.NewResolver(nil)
+//	if err != nil {
+//		log.Println("Failed to initialize resolver:", err.Error())
+//		os.Exit(1)
+//	}
+//
+//	results := make(chan *bonjour.ServiceEntry)
+//
+//	go func(results chan *bonjour.ServiceEntry, exitCh chan<- bool) {
+//		for e := range results {
+//			fmt.Printf("%v", e)
+//			action.CliContext.Set("host", e.HostName)
+//			action.CliContext.Set("port", string(e.Port))
+//			action.receive()
+//			exitCh <- true
+//			time.Sleep(1e9)
+//			os.Exit(0)
+//		}
+//	}(results, resolver.Exit)
+//
+//	err = resolver.Lookup("graft", "_graft._tcp", "", results)
+//	if err != nil {
+//		log.Println("Failed to browse:", err.Error())
+//	}
+//	select {}
+//}
