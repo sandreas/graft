810764ac7c86a150ebd6306c7ac1fff13fc3872e
andreas <andreas.schroden@gmail.com>
2017-09-12 07:39:32 +0200

    Fixed wrong pattern handling in strategy

--- transfer/strategy_test.go
+++ transfer/strategy_test.go
@@ -52,6 +52,9 @@ func TestComplexRelativeMapping(t *testing.T) {
 
 	strategy = prepareStrategy("src/test-dir", "C:/TestMissingDir")
 	expect.Equal("C:"+sep+"TestMissingDir"+sep+"test-dir"+sep+"test-dir-file.txt", strategy.DestinationFor("src/test-dir/test-dir-file.txt"))
+
+	strategy = prepareStrategy("*", ".")
+	expect.Equal("."+sep+"file", strategy.DestinationFor("file"))
 }
 
 func prepareStrategy(src string, dst string) *transfer.Strategy {
