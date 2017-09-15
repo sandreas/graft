83c415d284606385540e85b421e55c69012f5292
Andreas <andreas.schroden@gmail.com>
2017-09-15 13:32:05 +0200

    Fixed destination_test to expect path separator at the end

--- pattern/destination_test.go
+++ pattern/destination_test.go
@@ -1,21 +1,22 @@
 package pattern_test
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
-	"github.com/sandreas/graft/testhelpers"
-	"github.com/sandreas/graft/pattern"
 	"os"
+	"testing"
+
+	"github.com/sandreas/graft/pattern"
+	"github.com/sandreas/graft/testhelpers"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewDestinationPattern(t *testing.T) {
 	expect := assert.New(t)
 
 	mockFs := testhelpers.MockFileSystem(map[string]string{
-		"data/tmp/":         "",
+		"data/tmp/": "",
 	})
 	sep := string(os.PathSeparator)
 	destinationPattern := pattern.NewDestinationPattern(mockFs, "data/tmp/new$1_file")
-	expect.Equal("data" +sep+"tmp", destinationPattern.Path)
+	expect.Equal("data"+sep+"tmp"+sep, destinationPattern.Path)
 	expect.Equal("new${1}_file", destinationPattern.Pattern)
 }
